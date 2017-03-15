b8e85eed40e49da90edab8feb0b17a552a1cc86f
Hung Tran <hung@0x0.ninja>
2017-03-15 03:14:44 -0500

    add postgresql backup script

---
+++ s3-postgresql.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# PostgreSQL to S3
+#
+# This script performs the following task:
+# - obtains a list of databases on the specified server
+# - iterates through list to perform a database dump
+# - pipes the dump into gzip for compression
+# - pipes the compressed file to s3
+#
+# Note: The files will be uploaded to the following constructed path on S3
+#   s3://bucket/database/postgres/2017/03/15/1489558714-database.sqlc
+
+# configurable variables
+POSTGRES_HOST=127.0.0.1
+POSTGRES_PORT=5432
+POSTGRES_USER=****
+POSTGRES_PASS=****
+S3_BUCKET_NAME=****
+S3_BACKUP_TYPE=database/postgres
+
+# binary file paths
+PSQL_BIN=/usr/bin/psql
+PSQL_DMP=/usr/bin/pg_dump
+
+# export password
+export PGPASSWORD=${POSTGRES_PASS}
+
+# timestamp breakdown path
+TIMESTAMP=$(date +"%Y/%m/%d/%s")
+
+# fetch name of each database for individual backup
+DATABASES=`${PSQL_BIN} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER} -c "SELECT datname FROM pg_database WHERE NOT datistemplate" -tA | grep -Ev "(postgres)"`
+
+# process each database for backup
+echo "Uploading Compressed Backups..."
+for DATABASE in ${DATABASES}; do
+  ${PSQL_DMP} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER} --format=c ${DATABASE} | s3cmd put - s3://${S3_BUCKET_NAME}/${S3_BACKUP_TYPE}/${TIMESTAMP}-${DATABASE}.sqlc
+done
+echo "Done!"
+
+# remove password
+unset PGPASSWORD
